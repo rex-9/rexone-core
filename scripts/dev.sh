ac7f728ea9b169693125129c3efd3e57b228d593
Rex <htetnaing0814@gmail.com>
2026-07-07 22:11:40 +0700

    improve env: dev and prod with docker

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-# Check if the containers are already running
-if [ "$(docker-compose ps -q)" ]; then
-  echo "Containers are already running. Starting without rebuild..."
-  docker-compose -f docker-compose.dev.yaml up
-else
-  echo "Containers are not running. Building and starting..."
-  docker-compose -f docker-compose.dev.yaml up --build
-fi
+docker compose -f docker-compose.dev.yaml up --build
