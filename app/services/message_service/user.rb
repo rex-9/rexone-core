2f7601a24876b7463bb69222eb1a0608273183dc
naywin <170845045+naywin101@users.noreply.github.com>
2026-08-28 01:51:24 +0700

    bug fix

--- app/services/message_service/user.rb
+++ app/services/message_service/user.rb
@@ -1,5 +1,6 @@
 module MessageService
   class User < Base
+    USERS_RETRIEVED = "user.users_retrieved"
     CURRENT_FETCHED = "user.current_fetched"
     NOT_AUTHENTICATED = "user.not_authenticated"
     CURRENT_NOT_FOUND = "user.current_not_found"
